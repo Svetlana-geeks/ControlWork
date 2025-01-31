9225c5505973bcffe799924576f4febd798477c9
Svetlana-geeks <svetani.sn@gmail.com>
2025-01-31 08:10:08 +0600

    Added Inherited

--- OOP.py
+++ OOP.py
@@ -3,7 +3,8 @@
 #У класса должен быть приватный атрибут _age (возраст).
 class Person:
     def __inint__(self, name):
-        """ создает возрастб по умолчанию =0 """
+        self.name = name
+        """ создает возраст, по умолчанию =0 """
         self._age=0
 
 # Создайте методы для установки (set_age(age)) и получения (get_age()) возраста
@@ -24,3 +25,35 @@ print(p.get_age())  # Вывод: 25
 p.set_age(-5)  # Вывод: Ошибка: возраст не может быть отрицательным.
 print(p.get_age()) # Вывод: 25 (возраст не изменился)
 
+# Наследование
+# Создайте класс Animal и два наследника: Dog и Cat
+
+# У класса Animal должен быть атрибут name и метод speak(), который возвращает строку "I am an animal"
+class Animal:
+    def __init__(self,animal_name):
+        self.animal_name = animal_name
+
+    def speak(self):
+        print(f"{self.animal_name} I am an animal")
+
+#У класса Dog метод speak() должен возвращать "Woof".
+class Dog(self, Animal):
+    super().__init__(dog_name)  #  конструктор родительского класса
+    self.dog_name =dog_name
+
+    def speak(self):
+        print(f"{self.animal_name} Woof")
+
+#У класса Cat метод speak() должен возвращать "Meow"
+class Cat(Animal):
+    def __init__(self, cat_name):
+        self.dog_name = cat_name
+
+    def speak(self):
+        print(f"{self.animal_name} Meow")
+
+# Экзкмпляры и использование методов классов
+dog = Dog ("Mukhtar")
+print (dog, dog.speak(dog))
+cat = Cat ("Murzik")
+print (cat, cat.speak(cat))
